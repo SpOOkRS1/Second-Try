44b3af5cfda3d721e59fb31898a7e11869a80783
Insensual <>
2021-05-19 15:24:20 +0000

    G- can now create and display announcements

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for
 from flask_login import login_required, current_user
-from .models import Maid
+from .models import Maid, User
 from . import db
 import json
 import datetime
@@ -12,6 +12,14 @@ def home():
   currentMonth = datetime.datetime.now()
   thisMonth = currentMonth.strftime("%B")
   return render_template("home.html", user=current_user, mquery=Maid.query.all(), thisMonth = thisMonth)
+
+@views.route('/announcements')
+def ATTN():
+  currentMonth = datetime.datetime.now()
+  thisMonth = currentMonth.strftime("%B")
+  currentDate = datetime.datetime.now()
+  currDate = currentDate.strftime("%b, %d")
+  return render_template("announ.html", user=current_user, annList=User.query.all(), thisMonth = thisMonth, currDate = currDate)
 
 @views.route('/add', methods=['GET', 'POST', 'DELETE'])
 @login_required
@@ -34,6 +42,27 @@ def add():
         db.session.commit()
 
     return render_template("add.html", user=current_user, mquery=Maid.query.all(), thisMonth = thisMonth)
+
+
+@views.route('/create', methods=['GET', 'POST', 'DELETE'])
+@login_required
+def create():
+    currentMonth = datetime.datetime.now()
+    thisMonth = currentMonth.strftime("%B")
+    currentDate = datetime.datetime.now()
+    currDate = currentDate.strftime("%b, %d")
+    if request.method == 'POST':
+      announcement = request.form.get('announcement')
+
+      if len(announcement) < 1:
+        flash('Chore is too short!', category='error')
+      
+      else:
+        newAnn = User(announcement=announcement)
+        db.session.add(newAnn)
+        db.session.commit()
+
+    return render_template("createAnnoun.html", user=current_user, annList=User.query.all(), thisMonth = thisMonth, currDate = currDate)
 
 
 @views.route('/clear', methods=['GET', 'POST', 'DELETE'])
